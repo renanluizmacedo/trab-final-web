b2724fe965f46f3d29a3a812892c8f992fa7d7c9
renanluizmacedo <renanluizmacedo@hotmail.com>
2023-12-08 19:25:31 -0300

    versao final

--- js/scripts.js
+++ js/scripts.js
@@ -1,56 +1,3 @@
-/*!
- * Start Bootstrap - Freelancer v7.0.7 (https://startbootstrap.com/theme/freelancer)
- * Copyright 2013-2023 Start Bootstrap
- * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
- */
-//
-// Scripts
-//
-
-window.addEventListener("DOMContentLoaded", (event) => {
-    // Navbar shrink function
-    var navbarShrink = function () {
-        const navbarCollapsible = document.body.querySelector("#mainNav");
-        if (!navbarCollapsible) {
-            return;
-        }
-        if (window.scrollY === 0) {
-            navbarCollapsible.classList.remove("navbar-shrink");
-        } else {
-            navbarCollapsible.classList.add("navbar-shrink");
-        }
-    };
-
-    // Shrink the navbar
-    navbarShrink();
-
-    // Shrink the navbar when page is scrolled
-    document.addEventListener("scroll", navbarShrink);
-
-    // Activate Bootstrap scrollspy on the main nav element
-    const mainNav = document.body.querySelector("#mainNav");
-    if (mainNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: "#mainNav",
-            rootMargin: "0px 0px -40%",
-        });
-    }
-
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector(".navbar-toggler");
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll("#navbarResponsive .nav-link")
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener("click", () => {
-            if (window.getComputedStyle(navbarToggler).display !== "none") {
-                navbarToggler.click();
-            }
-        });
-    });
-
-
-});
 function fetchData() {
     const headers = new Headers({
         "Content-Type": "application/json",
@@ -85,14 +32,13 @@ function postData(formData) {
         .then((response) => response.json())
         .then((data) => {
             criarCards(data, 0);
+            window.alert("BEM VINDO A EMPRESA");
         })
         .catch((error) => {
             console.error("Error:", error);
         });
 }
-
 const form = document.getElementById("contactForm");
-
 form.addEventListener("submit", function (event) {
     event.preventDefault();
 
@@ -165,9 +111,7 @@ function excluirElemento(elementoId) {
     })
         .then((response) => {
             if (!response.ok) {
-                throw new Error(
-                    `Erro ao excluir elemento: ${response.status}`
-                );
+                throw new Error(`Erro ao excluir elemento: ${response.status}`);
             }
 
             const elementoRemovido = document.getElementById(
@@ -175,14 +119,13 @@ function excluirElemento(elementoId) {
             );
             if (elementoRemovido) {
                 elementoRemovido.remove();
+                window.alert("Vinculo com a empresa removido");
             } else {
                 console.warn(
                     `Elemento com ID ${elementoId} não encontrado na visualização.`
                 );
             }
         })
-        .catch((error) =>
-            console.error("Erro durante a exclusão:", error)
-        );
+        .catch((error) => console.error("Erro durante a exclusão:", error));
 }
 fetchData();
